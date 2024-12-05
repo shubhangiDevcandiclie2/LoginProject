a5a5c386e6c8e8355895ecc379e1f5db29209713
shubhangiDevcandiclie2 <shubhangi.devcandiclie@gmail.com>
2024-12-05 18:12:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection
